b4b1b7ce8d88b103731c4cf4e06fd3b7f05b7f29
brunojamelli <brunojamelli09@gmail.com>
2024-09-30 00:38:29 -0300

    feat: adição de mais opções de classes stubs

---
+++ External/Stub/EstoqueExternalStubSempre.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Domain.DTO;
+
+namespace eCommerce.External.Stub
+{
+    public class EstoqueExternalStubSempre : IEstoqueExternal
+    {
+        public EstoqueBaixaDTO DarBaixa(List<long> produtosIds, List<long> produtosQuantidades)
+        {
+            return new EstoqueBaixaDTO(true);
+        }
+        public DisponibilidadeDTO VerificarDisponibilidade(List<long> produtosIds, List<long> produtosQuantidades)
+    {
+        return new DisponibilidadeDTO(true, new List<long>());
+    }
+    }
+}
---
+++ External/Stub/PagamentoSucessoServiceStub.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Domain.DTO;
+
+namespace eCommerce.External.Stub
+{
+    public class PagamentoSucessoServiceStub : IPagamentoExternal
+    {
+        public PagamentoDTO AutorizarPagamento(long clienteId, double custoTotal)
+        {
+            bool autorizado = true;
+            long transacaoId = autorizado ? DateTime.Now.Ticks : 0;
+
+            return new PagamentoDTO(autorizado, transacaoId);
+        }
+
+        public void CancelarPagamento(long clienteId, long pagamentoTransacaoId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
